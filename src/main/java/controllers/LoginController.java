450f53cac64d36fd315e42a2c673315e0fe44df1
Popovici <raulpopovicilucian@gmail.com>
2021-03-25 22:47:02 +0200

    the pop up on the login window is initialised

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,23 +1,51 @@
 package controllers;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
+
+
+public class LoginController{
 
     @FXML
     private Button loginButton;
 
-    public void loginButtonAction(ActionEvent event){
+    @FXML
+    private Button registerButton;
 
-        int ok=0;
-        if(ok == 1){
-            Stage stage = (Stage) loginButton.getScene().getWindow();
-            stage.close();
-        }
+
+    public void loginButtonAction(ActionEvent event) throws IOException{
+
+
+    }
+    public void registerButtonAction(ActionEvent event) throws IOException{
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/register.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),823,1030);
+            Stage stage1 = new Stage();
+            stage1.setTitle("Register");
+            stage1.setScene(scene);
+            stage1.setResizable(false);
+            stage1.show();
+
+
+
+
+
 
 
     }
