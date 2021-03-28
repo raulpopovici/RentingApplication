e6c04b547b9944224f7157a3f207df8de8ae45da
Iof-Victor <vioftor@gmail.com>
2021-03-28 21:10:37 +0300

    the pop up for login is done

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -8,6 +8,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
@@ -25,12 +28,26 @@ public class LoginController{
 
     @FXML
     private Button registerButton;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Label wronglogin;
 
 
     public void loginButtonAction(ActionEvent event) throws IOException{
-
+                checklogin();
 
     }
+    public void checklogin() throws  IOException{
+            if(username.getText().isEmpty() && password.getText().isEmpty())
+            {
+                wronglogin.setText("Please enter your credentials ");
+            }
+    }
+
     public void registerButtonAction(ActionEvent event) throws IOException{
 
             FXMLLoader fxmlLoader = new FXMLLoader();
