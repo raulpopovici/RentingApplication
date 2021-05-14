2125f7b2f9b9831057a7f749443530cc6cc75484
Popovici <raulpopovicilucian@gmail.com>
2021-05-14 20:29:23 +0300

    new tests added

--- src/main/java/services/FileSystemService.java
+++ src/main/java/services/FileSystemService.java
@@ -5,8 +5,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileSystemService {
+
     public static  String APPLICATION_FOLDER = ".rentingapplication";
+
     private static final String USER_FOLDER = System.getProperty("user.home");
+
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
     public static Path getPathToFile(String... path) {
